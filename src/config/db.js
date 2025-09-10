cfb8ec8faaa6445f78d2b6e219ae29cb726f213b
EduardoLovo <eduardo.llovo@gmail.com>
2025-09-10 11:50:24 -0300

    db atualizado

--- src/config/db.js
+++ src/config/db.js
@@ -4,21 +4,41 @@ if (process.env.NODE_ENV !== 'production') {
 
 const mongoose = require('mongoose');
 
+// Variável para armazenar a conexão do banco de dados em cache
+let cachedDb = null;
+
 // Credenciais
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
 // Conexão com banco de dados MongoDB Atlas
-function connectToDatabase() {
-    mongoose
-        .connect(
+async function connectToDatabase() {
+    // Se a conexão já estiver em cache, retorne-a imediatamente
+    if (cachedDb) {
+        console.log('Usando a conexão do banco de dados em cache');
+        return cachedDb;
+    }
+
+    try {
+        // Se não houver conexão em cache, crie uma nova
+        const db = await mongoose.connect(
             `mongodb+srv://${dbUser}:${dbPass}@cluster0.9qskv.mongodb.net/`,
             {
                 dbName: `Inphantil`,
+                // Opções adicionais para otimizar a conexão
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
             }
-        )
-        .then(() => console.log('Conectado ao MongoDB'))
-        .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
+        );
+
+        // Armazene a nova conexão em cache
+        cachedDb = db;
+        console.log('Conectado a um novo banco de dados');
+        return cachedDb;
+    } catch (err) {
+        console.error('Erro ao conectar ao MongoDB:', err);
+        throw new Error('Falha na conexão do banco de dados.');
+    }
 }
 
 module.exports = connectToDatabase;
