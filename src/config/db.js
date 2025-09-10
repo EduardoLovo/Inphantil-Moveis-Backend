d9996652ce02752e952742257fe0cc6b399ef356
EduardoLovo <eduardo.llovo@gmail.com>
2025-09-10 15:34:57 -0300

    Db alterado

--- src/config/db.js
+++ src/config/db.js
@@ -4,49 +4,26 @@ if (process.env.NODE_ENV !== 'production') {
 
 const mongoose = require('mongoose');
 
-// Cache global para a conexão
-let cached = global.mongoose;
+// Credenciais
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
 
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
-
+// Conexão com banco de dados MongoDB Atlas
 async function connectToDatabase() {
-    if (cached.conn) {
-        // Conexão já existe, retorna ela
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        const dbUser = process.env.DB_USER;
-        const dbPass = process.env.DB_PASS;
-
-        const MONGODB_URI = `mongodb+srv://${dbUser}:${dbPass}@cluster0.9qskv.mongodb.net/Inphantil?retryWrites=true&w=majority`;
-
-        // Adicione opções de conexão para melhor performance
-        const opts = {
-            bufferCommands: false,
-            maxPoolSize: 5, // Reduzir o pool
-            serverSelectionTimeoutMS: 3000, // 3 segundos
-            socketTimeoutMS: 5000, // 5 segundos
-            connectTimeoutMS: 3000, // 3 segundos
-        };
-
-        cached.promise = mongoose
-            .connect(MONGODB_URI, opts)
-            .then((mongoose) => {
-                return mongoose;
-            });
-    }
-
     try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        throw e;
+        await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPass}@cluster0.9qskv.mongodb.net/`,
+            {
+                dbName: `Inphantil`,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
+        console.log('Conectado ao MongoDB');
+    } catch (err) {
+        console.error('Erro ao conectar ao MongoDB:', err);
+        process.exit(1); // Encerra a aplicação em caso de erro
     }
-
-    return cached.conn;
 }
 
 module.exports = connectToDatabase;
